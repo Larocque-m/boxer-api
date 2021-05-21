dae6a0d2ee60fff074a88ffd64791db5ff32539b
Michael <mlarock711@comcast.net>
2021-05-21 01:04:10 -0400

    Index.js file to run express and use pug.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const fights = require('./fights')
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+
+app.get('/', (request, response) => {
+  response.render('index', { fights })
+})
+
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+
+app.listen(1338, () => {
+  console.log('Listening on 1338...')
+})
